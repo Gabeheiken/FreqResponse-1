1004fc44954587aed404004c27923c4578d42f43
Gabeheiken <56289260+Gabeheiken@users.noreply.github.com>
2022-09-13 12:02:06 -0700

    Add files via upload

---
+++ Gabe_Evaluation_60Hz.py
@@ -0,0 +1,331 @@
+import matplotlib as mlp
+import easygui
+import matplotlib.pyplot as plt
+from matplotlib.widgets import Cursor
+import numpy as np
+import pandas as pd
+import os
+
+
+def offlineEventDetection(archiveFreq):
+
+    # ============================= Parameters =================================
+    slew_window = 158       # window for calculating slew
+    point_separation = 3        # gap between two slew points to be compared
+    fore_point = point_separation - 1
+    slew_diff_thresh = 0.00000385         # slew diff between two slew points separated by "point_separation"
+    series_over = 6  # the number of times in a row that slew_diff_thresh is exceeded
+    event_thresh = 0.00002161       # deviation of slew at the point when series over is reached
+
+    # ==========================================================================
+
+    Freq = archiveFreq
+    events = []
+    over = 0
+    check = 0
+    sum_ts = []
+    sum_freq = []
+    sum_tsfreq = []
+    sum_ts2 = []
+    slew = []
+    intercept = []
+    line = []
+    ts_ph = np.linspace(0, len(Freq), len(Freq))
+    Dif_slew = []
+    uf = 0
+    of = 0
+
+    for j in range(slew_window, len(Freq)):
+        sum_ts.append(np.sum(ts_ph[j - slew_window:j]))
+        sum_freq.append(np.sum(Freq[j - slew_window:j]))
+        sum_tsfreq.append(np.sum(ts_ph[j - slew_window:j] * Freq[j - slew_window:j]))
+        sum_ts2.append(np.sum(ts_ph[j - slew_window:j] * ts_ph[j - slew_window:j]))
+        slew.append((slew_window * sum_tsfreq[j-slew_window] - sum_ts[j-slew_window] * sum_freq[j-slew_window]) / (slew_window * sum_ts2[j-slew_window] - sum_ts[j-slew_window] * sum_ts[j-slew_window]))
+        intercept.append((sum_freq[j-slew_window] - slew[j-slew_window] * sum_ts[j-slew_window]) / slew_window)
+        line.append(slew[j-slew_window] * ts_ph[j-slew_window] + intercept[j-slew_window])
+
+        if j >= slew_window and j < (slew_window + point_separation):
+            continue
+
+        Dif_slew.append(abs(slew[j-slew_window] - slew[(j-slew_window) - fore_point]))
+
+        if len(Dif_slew) == 1:
+            continue
+
+        if Dif_slew[(j - slew_window) - point_separation] > slew_diff_thresh and Dif_slew[((j-slew_window) - point_separation) - 1] > slew_diff_thresh:
+            over = over + 1
+            check = 0
+        else:
+            check += 1
+
+        if check == 2:
+            over = 0
+
+        if over >= series_over and abs(slew[j - slew_window] - 0.00) >= event_thresh:
+            events.append(j)
+
+    return events
+
+
+# ========================  GETTING DATA FROM CSV FILES  ==============================
+
+#now use easygui library to open window so user can select event files to test with
+
+Archive_filePath = easygui.fileopenbox("Select input CSV File,then press open", "", filetypes= "*.csv",multiple=True)
+RTAC_filePath = easygui.fileopenbox("Select output CSV File,then press open", "", filetypes= "*.csv",multiple=True)
+
+is_event2 = []
+is_event3 = []
+
+k = 0
+for j in range(len(Archive_filePath)):
+    RTAC_TimeArr = []
+    Archive_FreqArr = []
+    RTAC_FreqArr = []
+    Archive_SlewArr = []
+    Archive_SlewVal = []
+    ArchiveSeriesEvents = []
+    Archive_FileSlewArr =[]
+    RTAC_SlewArr = []
+    RTAC_slewVal = []
+    RTAC_SeriesEventArr = []
+    RTACEventLine = np.nan
+
+    df = pd.read_csv(Archive_filePath[j], encoding='cp1252', low_memory=False)
+
+    df2 = pd.read_csv(RTAC_filePath[k], encoding='cp1252', low_memory=False)
+    df3 = pd.read_csv(RTAC_filePath[k+1], encoding='cp1252', low_memory=False)
+
+    i = 0
+    for i, row in df.iterrows():
+
+        Archive_FreqArr.append(df.iloc[i, 0])
+        print(Archive_FreqArr[i])
+        # Archive_FileSlewArr.append(df.iloc[i, 8])
+
+    length = len(Archive_FreqArr)
+    # for i in range(17999):
+    #     if i > length:
+    #         Archive_FreqArr.append(60)
+
+    ts_ph = np.linspace(0, len(Archive_FreqArr), len(Archive_FreqArr))
+    # Archive_SlewArr = slew_calc(228, ts_ph, Archive_FreqArr, Archive_filePath[j])
+
+
+    for i, row in df2.iterrows():
+        RTAC_FreqArr.append(df2.iloc[i, 1])
+        # RTAC_SlewVal = df2.iloc[i, 2]
+
+        # checks for string characters in slew
+        # RTAC_SlewVal = str(RTAC_SlewVal)
+        # if RTAC_SlewVal[-1].isdigit() == False:
+        #     RTAC_SlewVal = RTAC_SlewVal[:-1]
+        # RTAC_SlewArr.append(float(RTAC_SlewVal))
+        #
+        RTAC_SeriesEventArr.append(df2.iloc[i,3])
+
+    for i, row in df3.iterrows():
+        RTAC_FreqArr.append(df3.iloc[i, 1])
+        # RTAC_SlewVal = df2.iloc[i, 2]
+
+        # checks for string characters in slew
+        # RTAC_SlewVal = str(RTAC_SlewVal)
+        # if RTAC_SlewVal[-1].isdigit() == False:
+        #     RTAC_SlewVal = RTAC_SlewVal[:-1]
+        # RTAC_SlewArr.append(float(RTAC_SlewVal))
+        #
+        RTAC_SeriesEventArr.append(df3.iloc[i, 3])
+
+    for i in range(len(RTAC_FreqArr)):
+        if i > length:
+            RTAC_FreqArr.pop(length)
+
+    # for i in range(228-1):
+    #     Archive_SlewArr.insert(0, 0)
+    #     RTAC_SlewArr.insert(0, 0)
+
+
+    # for i in range(len(Archive_FileSlewArr)):
+    #     Archive_FileSlewArr[i] = Archive_FileSlewArr[i]/100
+
+
+# ==================  OFFLINE EVENT DETECTION  ============================
+
+    ArchiveSeriesEvents = offlineEventDetection(Archive_FreqArr)
+
+    if len(ArchiveSeriesEvents) > 0:
+        is_event2.append(True)
+    else:
+        is_event2.append(False)
+
+# ==================  ONLINE EVENT DETECTION  =============================
+
+    RTACEventLine = []
+    for i in range(len(RTAC_SeriesEventArr)):
+        if (i > 500) & (RTAC_SeriesEventArr[i] == 1.0) & (i < length - 500):  # ignore first 300 indexes and last 200, find first series event, break to ignore any after that
+            RTACEventLine.append(i)
+
+    if len(RTACEventLine) > 0:
+        is_event3.append(True)
+    else:
+        is_event3.append(False)
+
+# ================== PLOTTING ONLINE VS ONLINE FREQUENCY AND DETECTION TIMES =======================
+
+    fig, (ax1,ax2) = plt.subplots(2, 1, figsize=(15, 8.33))
+
+    # Define Archive Frequency graph
+    ax1.plot(Archive_FreqArr, color='k',lw=1)
+    ax1.set_ylim(59.8, 60.2)
+    ax1.title.set_text('Archive Data')
+    ax1.set_ylabel('Frequency (Hz)')
+    ax1.grid(True, color='k', linestyle=':')
+
+    if len(ArchiveSeriesEvents) > 0:
+        eventLine1 = ArchiveSeriesEvents[0]
+        ax1.vlines(x=[eventLine1], ymin=0, ymax=70, ls='--',color='r')
+
+    # Define cursor for finding the start of event
+    cursor1 = Cursor(ax1, horizOn=False, vertOn=True, useblit=True)
+
+    # ax2.plot(Archive_SlewArr)
+    # Define RTAC frequency graph
+    ax2.plot(RTAC_FreqArr, color='r',lw=1)
+    ax2.set_ylim(59.8, 60.2)
+    ax2.title.set_text('RTAC Data')
+    ax2.set_ylabel('Frequency (Hz)')
+    ax2.grid(True, color='k', linestyle=':')
+
+    if len(RTACEventLine) > 0:
+        eventLine2 = RTACEventLine[0]
+        ax2.vlines(x=[eventLine2], ymin=0, ymax=70,
+                   ls='--',color='r')  # this is vertical line showing where RTAC event flag is seen
+
+    # Define cursor for finding the start of event
+    cursor2 = Cursor(ax2, horizOn=False, vertOn=True, useblit=True)
+
+    Inception = []
+    def onclick(event):
+        global Inception
+        Inception = int(event.xdata)
+        print(Inception)
+        dt = (RTACEventLine - Inception) / 30
+        print('dt: ',dt)
+
+    fig.canvas.mpl_connect('button_press_event', onclick)
+
+    # print('\nOffline events detected: ', is_event2)
+    # print('Online events detected: ', is_event3)
+
+    dir_Path = "Z:\Test results\Output tests\Plots"
+    my_file = 'graph' + str(j) + '.png'
+
+    fig.savefig(os.path.join(dir_Path, my_file))
+
+    plt.show()
+
+    k = k + 2
+
+
+
+# ====================== Evaluation code =======================================
+
+# home_dir=r'E:\Study\Engineering\MS USA\Power Engineering Lab\Fresp\2021 algo\\'     # Project home dir
+human_valid='Z:\Human Validation - 30.csv'        # human validation file containing expert assessment
+df3 = pd.read_csv(human_valid)       # read human validation .csv
+name = df3['Name']        # read first column with names of original freq files
+is_eventH = df3['Is_event']       # read last column that consists of event assessment i.e. True/False
+print('\nOffline events detected: ', is_event2)
+print('Online events detected: ', is_event3)
+print(is_eventH)
+# Paths for original frequency files
+files_path=r'Z:\20 File Set'
+
+
+# print('\nOffline events detected: ', is_event2)
+# print('Online events detected: ', is_event3)
+print('\n\n===== Offline Evaluation =====')
+TP=0
+TN=0
+FP=0
+FN=0
+for i in range(len(is_event2)):
+    if is_event2[i] == is_eventH[i] and is_event2[i] == True:
+        TP=TP+1
+    elif is_event2[i] == is_eventH[i] and is_event2[i] == False:
+        TN=TN+1
+    elif is_event2[i] != is_eventH[i] and is_event2[i] == True:
+        FP=FP+1
+    elif is_event2[i] != is_eventH[i] and is_event2[i] == False:
+        FN=FN+1
+
+
+totalexample = TP + FP + FN + TN
+print("TP = {}\nFP = {}\nFN = {}\nTN = {}\nTotal examples = {}".format(TP, FP, FN, TN, totalexample))
+Accuracy = round(((TP + TN) / totalexample) * 100)
+if TP == 0 and FN == 0:
+    FN = totalexample
+Sensitivity = (TP / (TP + FN)) * 100
+if TP == 0 and FP == 0:
+    FP = totalexample
+Precision = round((TP / (TP + FP)) * 100)
+if TN == 0 and FP == 0:
+    FP = totalexample
+Specificity = round((TN / (TN + FP)) * 100)
+FDR = round((FP / (FP + TP)) * 100)
+# calculating the F scores
+print('Accuracy:',Accuracy)
+print('Sensitivity:',Sensitivity)
+print('Precision:',Precision)
+print('Specificity:',Specificity)
+print('FDR:',FDR)
+
+
+# print(detect)             # print the sample at which event is detected in each file
+# print(is_event2)           # print event classification by the algorithm
+# print(list(is_eventH))        # print event classification by the human experts
+# end = time.time()
+# print('Time elapsed:', end-start)
+
+
+print('\n\n===== Online Evaluation =====')
+TP=0
+TN=0
+FP=0
+FN=0
+for i in range(len(is_event2)):
+    if is_event3[i] == is_eventH[i] and is_event3[i] == True:
+        TP=TP+1
+    elif is_event3[i] == is_eventH[i] and is_event3[i] == False:
+        TN=TN+1
+    elif is_event3[i] != is_eventH[i] and is_event3[i] == True:
+        FP=FP+1
+    elif is_event3[i] != is_eventH[i] and is_event3[i] == False:
+        FN=FN+1
+
+
+totalexample = TP + FP + FN + TN
+print("TP = {}\nFP = {}\nFN = {}\nTN = {}\nTotal examples = {}".format(TP, FP, FN, TN, totalexample))
+Accuracy = round(((TP + TN) / totalexample) * 100)
+if TP == 0 and FN == 0:
+    FN = totalexample
+Sensitivity = (TP / (TP + FN)) * 100
+if TP == 0 and FP == 0:
+    FP = totalexample
+Precision = round((TP / (TP + FP)) * 100)
+if TN == 0 and FP == 0:
+    FP = totalexample
+Specificity = round((TN / (TN + FP)) * 100)
+FDR = round((FP / (FP + TP)) * 100)
+# calculating the F scores
+print('Accuracy:',Accuracy)
+print('Sensitivity:',Sensitivity)
+print('Precision:',Precision)
+print('Specificity:',Specificity)
+print('FDR:',FDR)
+
+# print(detect)             # print the sample at which event is detected in each file
+# print(is_event2)           # print event classification by the algorithm
+# print(list(is_eventH))        # print event classification by the human experts
+# end = time.time()
+# print('Time elapsed:', end-start)
---
+++ Gabe_GS_60Hz.py
@@ -0,0 +1,159 @@
+import csv
+import os
+import socket  # Import libraries
+import time
+from time import sleep
+from timer import timer
+
+# import GS_computer as catch
+import easygui
+
+
+# Gabe's method for sending archive file to grid simulator when new RTAC recording has begun.
+# This code utilizes the unraid server that is connected to both the grid sim and the RTAC computer,
+# so there is no need to ping back and forth when a file has begun.
+# This code builds on the Umar_GS code
+
+def conn():
+    global s
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    s.settimeout(1000)
+    s.connect(('192.168.0.149', 5025))  # NHR 9410 IP address
+    output = 'SYST:RWL\n'  # Lock the touch panel screen (Safety purpose)
+    s.send(output.encode('utf-8'))  # For each SCPI command, it MUST encoded into utf-8.
+    return s
+
+
+def clos(s):  # This function is meant to close the connection
+    output5 = 'SYST:LOC\n'  # Unlock the touch panel screen
+    s.send(output5.encode('utf-8'))
+    s.close()  # Close the connection
+
+
+
+def Gs(file):
+    global length1
+    global j
+    global path
+    path = "Z:/RTAC_Recordings2"
+    arr = []
+    curr = time.time()
+    t = timer()
+    av = []
+
+
+
+    with open(f"{file}") as csv_file:
+        print(file)
+        csv_reader = csv.DictReader(csv_file, delimiter=',')
+        i = 1
+        for rows in csv_reader:
+            y = rows['STATION_1:Freq']
+            y = float(y) + 0.01203651
+            y = round(y, 6)
+            arr.append(y)
+
+    conn()
+    # s.send('FREQ 60.00 \n'.encode('utf-8'))
+    s.send('MACR :LEAR 1 \n'.encode('utf-8'))
+    # print("sent1")
+    s.send('MACR:OPER:SYNC:INST1 SYNC \n'.encode('utf-8'))
+    # print("sent2")
+    s.send('MACR:LEAR 0 \n'.encode('utf-8'))
+    # print("sent4")
+    s.send('MACR:RUN \n'.encode('utf-8'))
+    # print("sent5")
+    output2 = 'FREQ '
+    m = 1
+
+    # If file is less than 18000 lines, fills end with 60s
+    for i in range(36000):
+        if i > len(arr):
+            arr.append(60)
+
+    # while time.time() < (curr + 0.0): # 11.5   There is a delay before the next RTAC file begins recording
+    #     pass
+
+    for i in arr:
+        start_time = time.time()*1000
+        var = output2 + str(i) + '\n'
+        s.send(var.encode('utf-8'))
+        print('frequency value: ', i, ' in line ', m, '\n')
+        m = m + 1
+
+        while (time.time()*1000) < (start_time + 15.52): # 15.61
+            pass
+
+        # sleep(0.015)
+
+        # print('response: ', msg)
+        # print('File: ', j+1)
+
+        # if time.time() > (curr + 285) and time.time() < (curr + 295): #609.5
+        #     print('HERE!')
+        #     lis2 = os.listdir(path)
+        #     length2 = len(lis2)
+        #     print(length1)
+        #     print(length2)
+        #     if length2 > length1:
+        #         sleep(3)
+
+
+        tot = time.time() - curr
+        print('Total time: ', round(tot,3))
+        if time.time() > (curr + 570): #609.5
+            lis2 = os.listdir(path)
+            length2 = len(lis2)
+            print(length1)
+            print(length2)
+            if length2 > length1 + 1:
+                length1 = length2
+                break
+            print('Here')
+
+        tot1 = time.time() - start_time/1000
+        freq = 1 / tot1
+        av.append(freq)
+        print('Send freq: ', round(freq, 3))
+        print('Avg:', sum(av) / len(av))
+        if freq < 55:
+            print('SEND FREQUENCY TOO SLOW! RESTART TEST')
+
+        if m > 35995:
+            while True:
+                lis2 = os.listdir(path)
+                length2 = len(lis2)
+                if length2 > length1 + 1:
+                    length1 = length2
+                    break
+
+    clos(s)
+
+
+filePath = easygui.fileopenbox("Select archive file to send: ", "", filetypes="*.csv", multiple=True)
+print(filePath)
+path = "Z:/RTAC_Recordings2"
+lis = os.listdir(path)
+length = len(lis)
+
+# ans = input('Would you like to run another test? y/n \n')
+# if ans == 'y':
+#     filePath2 = easygui.fileopenbox("Select archive file to send: ", "", filetypes="*.csv", multiple=True)
+#     filePath.append(filePath2)
+
+print('Waiting for new RTAC recording file...')
+
+
+while True:
+    lis1 = os.listdir(path)
+    length1 = len(lis1)
+    if length < length1:
+        start = time.time()
+        for j in range(len(filePath)):
+            Gs(filePath[j])
+
+        break
+
+
+
+
